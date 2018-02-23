b08bc003c9d36301aff328cb0f2ef0e8667b037b
bigbug <15893969@qq.com>
2018-02-23 08:13:37 +0800

    批量更新

--- sites/all/modules/cowpro/modules/rewards/cowpro-rewards-february.tpl.php
+++ sites/all/modules/cowpro/modules/rewards/cowpro-rewards-february.tpl.php
@@ -1,14 +1,14 @@
 <h2>一级奖励推荐列表</h2>
-<ul> 
+<ul>
 
 <?php foreach ($bount1 as $row): ?>
-    <li class=""><span><?php print $row['name'];?></span>&nbsp;&nbsp;&nbsp;&nbsp;<span>奖金：<?php print $row['amount'];?>元</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>代金卷：<?php print $row['voucher'];?>元</span></li>
+    <li class=""><span><?php print $row['name'];?></span>&nbsp;&nbsp;&nbsp;&nbsp;<span>奖金：<?php print $row['amount'];?>元</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>代金券：<?php print $row['voucher'];?>元</span></li>
  <?php endforeach; ?>
 </ul>
 
 
 <h2>二级奖励推荐列表</h2>
-<ul> 
+<ul>
 
 <?php foreach ($bount2 as $row): ?>
     <li class=""><span><?php print $row['name'];?></span>&nbsp;&nbsp;&nbsp;&nbsp;<span><?php print $row['amount'];?>元</span></li>
