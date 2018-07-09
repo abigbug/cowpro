5e163c961e92501be472aa2b9e5de462ad5a3f13
DostarQ <40755334+DostarQ@users.noreply.github.com>
2018-07-09 16:02:10 +0800

    Add files via upload
    修改微信底部JS报错问题

--- sites/all/themes/cowpro_p2p_v2/js/cowpro_p2p.core.js
+++ sites/all/themes/cowpro_p2p_v2/js/cowpro_p2p.core.js
@@ -29,8 +29,9 @@ if (!Array.prototype.indexOf) {
 	};
 }
 
+
 // jQuery Browser Detect Tweak For IE7
-jQuery.browser.version = jQuery.browser.msie && parseInt(jQuery.browser.version) == 6 && window["XMLHttpRequest"] ? "7.0" : jQuery.browser.version;
+//jQuery.browser.version = jQuery.browser.msie && parseInt(jQuery.browser.version) == 6 && window["XMLHttpRequest"] ? "7.0" : jQuery.browser.version;
 
 // Console.log wrapper to avoid errors when firebug is not present
 // usage: log('inside coolFunc', this, arguments);
@@ -59,3 +60,103 @@ cowpro_p2p.image.preload = function() {
 		cowpro_p2p.image._cache.push(cacheImage);
 	}
 }
+
+jQuery(document).ready(function($){
+    $(".boxflex li a").each(function(){
+        $this = $(this);
+        if($this[0].href==String(window.location)){  
+            $(".boxflex li").removeClass("on");
+            $this.parent().addClass("on");  
+        }  
+    });
+        
+    var msg = $("div.messages");
+    if (msg.size() > 0) {
+            alert(msg.text());
+    }
+  
+  	$("#cowpro-fund-withdraw #edit-amount").blur(function(){
+      if( $(this).val() < 100 ) {
+        alert("提现金额小于100元时，收取1元手续费。");
+      }
+    });
+   
+});
+
+    window.alert = function(str)  
+    {  
+     var shield = document.createElement("DIV");  
+     shield.id = "shield";  
+     shield.style.position = "absolute";  
+     shield.style.left = "0px";  
+     shield.style.top = "0px";  
+     shield.style.width = "100%";  
+     shield.style.height = document.body.scrollHeight+"px";  
+     //弹出对话框时的背景颜色  
+     shield.style.background = "#e4e4e4";  
+     shield.style.textAlign = "center";  
+     shield.style.zIndex = "9999999";
+     
+     //背景透明 IE有效  
+     //shield.style.-moz-opacity = 0.8;  
+     shield.style.opacity = .80;  
+     shield.style.filter = "alpha(opacity=80)";  
+     var alertFram = document.createElement("DIV");  
+     alertFram.id="alertFram";  
+     alertFram.style.position = "absolute";  
+     alertFram.style.left = "10%";  
+     alertFram.style.top = "10%";  
+     alertFram.style.marginLeft = "0px";  
+     alertFram.style.marginTop = "0px";  
+     alertFram.style.width = "80%";  
+     alertFram.style.height = "150px";  
+     alertFram.style.background = "#ff0000";  
+     alertFram.style.textAlign = "center";  
+     alertFram.style.lineHeight = "150px";  
+     alertFram.style.zIndex = "99999999";  
+     strHtml = "<ul style=\"list-style:none;margin:0px;padding:0px;width:100%\">\n";  
+     strHtml += " <li style=\"background:#FFB226;text-align:left;padding-left:20px;font-size:14px;font-weight:bold;height:25px;line-height:25px;border:1px solid #F9CADE;\">[提示]</li>\n";  
+     strHtml += " <li style=\"background:#fff;text-align:center;font-size:12px;height:120px;line-height:100px;border-left:1px solid #FFB226;border-right:1px solid #F9CADE;\">"+str+"</li>\n";  
+     strHtml += " <li style=\"background:#FFB226;text-align:center;font-weight:bold;height:25px;line-height:25px; border:1px solid #F9CADE;\"><input type=\"button\" value=\"确 定\" onclick=\"doOk()\" /></li>\n";  
+     strHtml += "</ul>\n";  
+     alertFram.innerHTML = strHtml;  
+     document.body.appendChild(alertFram);  
+     document.body.appendChild(shield);  
+     //var ad = setInterval("doAlpha()",5);  
+     this.doOk = function(){  
+         alertFram.style.display = "none";  
+         shield.style.display = "none";  
+     }  
+     alertFram.focus();  
+     document.body.onselectstart = function(){return false;};  
+    }
+    
+    function show(id,id2,id3,obj){
+       jQuery(".boxflex li").removeClass("on");
+       obj.parent().addClass("on");
+       document.getElementById(id3).style.display="none";
+       if(id2=="Layer2"){
+          if(document.getElementById(id2).style.display=="block"){
+            document.getElementById(id).style.display="none";
+            document.getElementById(id2).style.display="none";
+          }else{
+            document.getElementById(id).style.display="block";
+            document.getElementById(id2).style.display="block";
+          }
+       }else if(id2=="Layer3"){
+          if(document.getElementById(id2).style.display=="block"){
+            document.getElementById(id).style.display="none";
+            document.getElementById(id2).style.display="none";
+          }else{
+            document.getElementById(id).style.display="block";
+            document.getElementById(id2).style.display="block";
+          } 
+       }
+    }
+    
+    function CloseDiv(show_div,bg_div)
+    {
+    document.getElementById(show_div).style.display='none';
+    document.getElementById(bg_div).style.display='none';
+    };
+
